8a8f6d2df6ecc67f920472e2bee56e9307d5c2f3
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-10-10 16:35:12 +0500

    api

--- src/components/contacts/GoogleMapComponent.tsx
+++ src/components/contacts/GoogleMapComponent.tsx
@@ -13,9 +13,9 @@ const GoogleMapComponent = () => {
 
   useEffect(() => {
     const loadGoogleMapsScript = () => {
-      if (!document.querySelector(`script[src="https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}"]`)) {
+      if (!document.querySelector(`script[src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZUgoIjDlwgVo3cuTWmAyl8HkIJoaioGc"]`)) {
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAZUgoIjDlwgVo3cuTWmAyl8HkIJoaioGc`;
         script.async = true;
         script.defer = true;
         script.onload = () => {
